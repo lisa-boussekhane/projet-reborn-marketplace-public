5ac4860c777fee2126e2db224ff4a3678a3c8f62
ritaverissimo86 <rita.verissimo.h@gmail.com>
2024-02-08 15:09:32 +0100

    routes_ajoutees

--- BACK/App/Router/router.js
+++ BACK/App/Router/router.js
@@ -8,4 +8,28 @@ const paymentController = require('./Controllers/paymentController');
 const router = express.Router();
 
 router.get('/user/:id', userController.getUserInfos);
-router.post('/lists', listController.createOneList);
+router.patch('/user/:id', userController.updateAccount);
+router.delete('/user/:id', userController.deleteAccount);
+router.post('/signup', userController.createUserAccount);
+router.post('/login', userController.logAccount);
+router.patch('/login', userController.updatePassword);
+router.get('/myaccount', userController.getUserDashboard);
+
+router.get('/result', searchController.searchReborns);
+
+router.post('/payment', paymentController.bankAccount);
+
+router.get('/product/:id', productController.productPage);
+router.post('/product/:id', productController.createProduct);
+router.get('/product', productController.productsPage);
+
+router.get('/shop/:id', shopController.showStore);
+router.post('/shop/:id', shopController.createShop);
+
+router.get('/chat/:id', chatController.getMessage);
+router.patch('/chat/message/room/:id', chatController.chatRoom);
+
+
+
+
+
