3e0141b826d85e80315150814d46aa7420fb1ee0
lisa-boussekhane <lisa.boussekhane@gmail.com>
2024-02-08 20:00:10 +0100

    oups

--- src/components/CreateMyStore/CreateMyStore.jsx
+++ src/components/CreateMyStore/CreateMyStore.jsx
@@ -4,46 +4,47 @@ export default function CreateMyStore() {
   return (
     <div className="cms">
       <h1>Create my store</h1>
-      <div className="cms-form">
-        <div className="cms-form_item cms-form_item_left">
-          <h2>Pick a name</h2>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            placeholder="Your name..."
-            required
-          />
-        </div>
-
-        <div className="cms-form_item cms-form_item_right">
-          <h2>Are you a professional or an individual?</h2>
-
-          <label htmlFor="professional">
+      <form>
+        <div className="cms-form">
+          <div className="cms-form_item cms-form_item_left">
+            <h2>Pick a name</h2>
             <input
-              type="radio"
-              id="professional"
-              name="user-type"
-              value="professional"
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Your name..."
               required
             />
-            Professional
-          </label>
+          </div>
 
-          <label htmlFor="individual">
-            <input
-              type="radio"
-              id="individual"
-              name="user-type"
-              value="individual"
-              required
-            />
-            Individual
-          </label>
+          <div className="cms-form_item cms-form_item_right">
+            <h2>Are you a professional or an individual?</h2>
+
+            <label htmlFor="professional">
+              <input
+                type="radio"
+                id="professional"
+                name="user-type"
+                value="professional"
+                required
+              />
+              Professional
+            </label>
+
+            <label htmlFor="individual">
+              <input
+                type="radio"
+                id="individual"
+                name="user-type"
+                value="individual"
+                required
+              />
+              Individual
+            </label>
+          </div>
         </div>
-      </div>
-
-      <button type="submit">Create my store</button>
+        <button type="submit">Create my store</button>
+      </form>
     </div>
   );
 }
