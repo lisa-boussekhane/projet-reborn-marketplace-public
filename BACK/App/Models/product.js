3c4f9c5f49c017802348358b39f26932f3637363
Sarah-Iritie <jarjenillustrations@gmail.com>
2024-02-09 10:40:13 +0100

    modif

--- BACK/App/Models/product.js
+++ BACK/App/Models/product.js
@@ -7,20 +7,14 @@ class product extends Model {}
 product.init(
   {
     security_code: {
-<<<<<<< HEAD
-      type: INTEGER,
+      type: DataTypes.UUID,
       allowNull: false,
-=======
-        type: DataTypes.UUID,
-        allowNull: false
->>>>>>> f76f0e69c457a4f779eaad1613c05f8680326e6a
     },
     title: {
-        type: DataTypes.STRING,
+      type: DataTypes.STRING,
       allowNull: false,
     },
     kit_name: {
-<<<<<<< HEAD
       type: VARCHAR,
       allowNull: false,
     },
@@ -61,46 +55,3 @@ product.init(
 );
 
 module.exports = product;
-=======
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      sculptor: {
-        type: INTEGER,
-        allowNull: false,
-      },
-      size: {
-        type: INTEGER,
-        allowNull: false
-      },
-      type: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      age_range: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      authenticity_card: {
-        type: BOOLEAN,
-        allowNull: false,
-      },
-      price: {
-        type: INTEGER,
-        allowNull: false,
-      },
-      shipping_fees: {
-        type: INTEGER,
-        allowNull: false,
-      },
-  
-  }, {  
-    sequelize,
-    modelName: 'product',
-    tableName: 'product',
-  });
-  
-  module.exports = product;
-
-  //blabla
->>>>>>> f76f0e69c457a4f779eaad1613c05f8680326e6a
