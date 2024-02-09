bb8da1507c1cfb092d04a7c243a10efe7fcc8a81
Sarah-Iritie <jarjenillustrations@gmail.com>
2024-02-09 09:57:23 +0100

    test

--- BACK/App/Models/product.js
+++ BACK/App/Models/product.js
@@ -1,55 +1,57 @@
 const { Model, DataTypes, INTEGER, BOOLEAN } = require('sequelize');
 
-const sequelize = require ('./sequelize');
+const sequelize = require('./sequelize');
 
 class product extends Model {}
 
-product.init({  
+product.init(
+  {
     security_code: {
       type: INTEGER,
-      allowNull: false
+      allowNull: false,
     },
     title: {
       type: VARCHAR,
       allowNull: false,
     },
     kit_name: {
-        type: VARCHAR,
-        allowNull: false
-      },
-      sculptor: {
-        type: INTEGER,
-        allowNull: false,
-      },
-      size: {
-        type: INTEGER,
-        allowNull: false
-      },
-      type: {
-        type: VARCHAR,
-        allowNull: false,
-      },
-      age_range: {
-        type: VARCHAR,
-        allowNull: false,
-      },
-      authenticity_card: {
-        type: BOOLEAN,
-        allowNull: false,
-      },
-      price: {
-        type: INTEGER,
-        allowNull: false,
-      },
-      shipping_fees: {
-        type: INTEGER,
-        allowNull: false,
-      },
-  
-  }, {  
+      type: VARCHAR,
+      allowNull: false,
+    },
+    sculptor: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    size: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    type: {
+      type: VARCHAR,
+      allowNull: false,
+    },
+    age_range: {
+      type: VARCHAR,
+      allowNull: false,
+    },
+    authenticity_card: {
+      type: BOOLEAN,
+      allowNull: false,
+    },
+    price: {
+      type: INTEGER,
+      allowNull: false,
+    },
+    shipping_fees: {
+      type: INTEGER,
+      allowNull: false,
+    },
+  },
+  {
     sequelize,
     modelName: 'product',
     tableName: 'product',
-  });
-  
-  module.exports = product;
+  }
+);
+
+module.exports = product;
