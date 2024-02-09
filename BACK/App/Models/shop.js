e0683950ff73852094f6ad59e0f6877bf3136b75
ritaverissimo86 <rita.verissimo.h@gmail.com>
2024-02-09 09:47:31 +0100

    shop

---
+++ BACK/App/Models/shop.js
@@ -0,0 +1,25 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require ('./sequelize');
+
+class shop extends Model {}
+
+shop.init({  
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    link_seller_profile: {
+        type: PATH,
+        allowNull: false
+      },
+  
+  }, {  
+    sequelize,
+    modelName: 'shop',
+    tableName: 'shop',
+  });
+  
+  module.exports = shop;
+
+  //bla
