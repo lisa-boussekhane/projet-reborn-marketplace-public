8a68157c395a999bde3103c2bee78cbe1c33cd64
ritaverissimo86 <rita.verissimo.h@gmail.com>
2024-02-12 16:27:34 +0100

    update_logAccount

--- BACK/App/Controllers/authController.js
+++ BACK/App/Controllers/authController.js
@@ -65,7 +65,27 @@ async createUserAccount(req, res){
         } catch (error) {
         res.status(500).json({ error: 'Registration failed' });
         }
+        },
+
+async logAccount (req, res){
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        if (!user) {
+        return res.status(401).json({ error: 'Authentication failed' });
         }
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) {
+        return res.status(401).json({ error: 'Authentication failed' });
+        }
+        const token = jwt.sign({ userId: user._id }, 'your-secret-key', {
+        expiresIn: '1h',
+        });
+        res.status(200).json({ token });
+    } catch (error) {
+       res.status(500).json({ error: 'Login failed' });
+        }
+}
 };
 
 module.exports = authController;
