5b58a51b84282906048218fd61e6018c8d925259
Sarah-Iritie <jarjenillustrations@gmail.com>
2024-02-12 18:59:33 +0100

    error

---
+++ src/components/MyStore/MyStore.jsx
@@ -0,0 +1,49 @@
+import './MyStore.scss';
+import { CardHeader, CardContent, Card, Icon, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+export default function MyStore({ counter }) {
+  return (
+    <>
+      <div className="seller__container">
+        <div className="seller__title">
+          <h2>You have {counter} Reborns on sale</h2>
+        </div>
+        <div className="seller__rating">
+          <h3>
+            Your seller's rating: <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+            <FontAwesomeIcon icon={faStar} />
+          </h3>
+        </div>
+      </div>
+      <div className="seller__btn">
+        <button type="submit">Add an article</button>
+      </div>
+      <div className="seller__card">
+        <Card>
+          {/* image à modifier plus tard */}
+          <Image
+            src="https://react.semantic-ui.com/images/avatar/large/matthew.png"
+            wrapped
+            ui={false}
+          />
+          <CardContent>
+            <CardHeader>Title</CardHeader>
+          </CardContent>
+          <CardContent extra>
+            {/* lien à modifier plus tard */}
+            <Link to="/">
+              <Icon name="edit" />
+              Edit
+            </Link>
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
+}
