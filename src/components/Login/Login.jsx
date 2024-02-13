f69deb2f67d06b99a9f5dd2aa30995f003b89af5
Sarah-Iritie <jarjenillustrations@gmail.com>
2024-02-13 13:31:27 +0100

    auth non reussie

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,19 +1,55 @@
 import './Login.scss';
-import { useState } from 'react';
+import { useNavigate, useLocation } from 'react-router';
+import { useState, useContext } from 'react';
+import { useAuth } from 'react-use-auth';
+import axios from 'axios';
 import { NavLink } from 'react-router-dom';
 
-export default function Login() {
-  const [email, setEmail] = useState('');
+const Login = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || '/login';
+  const { auth, setAuth } = useAuth();
+  const [name, setName] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-  function handleSubmit(e) {
+  const API = axios.create({
+    baseURL: 'http://localhost:3000',
+  });
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-  }
+    try {
+      const res = await API.post('/login', {
+        name,
+        password,
+      }).then((res) => {
+        if (res?.data.name) {
+          const role = res?.data.role;
+          setAuth({ role: `${role}`, name: `${name}` });
+          setName('');
+          setPassword('');
+          navigate(from, { replace: true });
+        } else {
+          console.log('incorrect submission');
+          setError(res.message);
+        }
+      });
+    } catch (err) {
+      if (!err?.response) {
+        setError('no server response');
+      } else {
+        setError('registration failed');
+      }
+    }
+  };
+
   return (
     <>
       <div className="form__container">
         <h1>Login</h1>
-        <form action="" method="post">
+        <form onSubmit={handleSubmit}>
           <div className="form__group">
             <label htmlFor="email">
               Email address:
@@ -24,13 +60,12 @@ export default function Login() {
                 value={email}
                 placeholder="Email address"
                 required
-                onChange={(e) => setEmail(e.target.value)}
               />
             </label>
           </div>
           <div className="form__group">
             <label htmlFor="password">
-              Password:{' '}
+              Password:
               <input
                 type="password"
                 name="password"
@@ -38,7 +73,6 @@ export default function Login() {
                 value={password}
                 placeholder="Password"
                 required
-                onChange={(e) => setPassword(e.target.value)}
               />
             </label>
           </div>
@@ -57,4 +91,6 @@ export default function Login() {
       </div>
     </>
   );
-}
+};
+
+export default Login;
