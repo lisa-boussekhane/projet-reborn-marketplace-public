7b1d4d29607dbc6cf13c7eb3f3c6a5aef32dbda8
lisa-boussekhane <lisa.boussekhane@gmail.com>
2024-02-13 14:34:57 +0100

    ajout-de-l'export

--- BACK/App/Middlewares/authMiddleware.js
+++ BACK/App/Middlewares/authMiddleware.js
@@ -1,17 +1,17 @@
 const jwt = require('jsonwebtoken');
 
 function verifyToken(req, res, next) {
-    const token = req.header('Authorization');
-    if (!token) return res.status(401).json({ error: 'Access denied' });
+  const token = req.header('Authorization');
+  if (!token) return res.status(401).json({ error: 'Access denied' });
 
-try {
+  try {
     const decoded = jwt.verify(token, 'your-secret-key');
     req.userId = decoded.userId;
     next();
- } catch (error) {
+  } catch (error) {
     res.status(401).json({ error: 'Invalid token' });
- }
- };
+  }
+}
 
 module.exports = verifyToken;
 
