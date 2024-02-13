050da1073f6ce217751128c82325da83e4568d07
lisa-boussekhane <lisa.boussekhane@gmail.com>
2024-02-13 14:10:21 +0100

    update

--- BACK/App/Models/index.js
+++ BACK/App/Models/index.js
@@ -1,25 +1,31 @@
-const user = require("./user");
-const detail_product = require("./detail_product");
-const media = require("./media");
-const product = require("./product");
-const shop = require("./shop");
+const user = require('./user');
+const detail_product = require('./detail_product');
+const media = require('./media');
+const product = require('./product');
+const shop = require('./shop');
 
 // User and Shop (One-to-Many)
-User.hasMany(Shop, { foreignKey: 'user_id' });
-Shop.belongsTo(User, { foreignKey: 'user_id' });
+user.hasMany(shop, { foreignKey: 'user_id' });
+shop.belongsTo(user, { foreignKey: 'user_id' });
 
 // Product and Shop (Many-to-One)
-Shop.hasMany(Product, { foreignKey: 'shop_id' });
-Product.belongsTo(Shop, { foreignKey: 'shop_id' });
+shop.hasMany(product, { foreignKey: 'shop_id' });
+product.belongsTo(shop, { foreignKey: 'shop_id' });
 
 // Product and User (Many-to-Many through User_Order_Product)
-Product.belongsToMany(User, { through: 'User_Order_Product', foreignKey: 'product_id' });
-User.belongsToMany(Product, { through: 'User_Order_Product', foreignKey: 'user_id' });
+product.belongsToMany(user, {
+  through: 'User_Order_Product',
+  foreignKey: 'product_id',
+});
+user.belongsToMany(product, {
+  through: 'User_Order_Product',
+  foreignKey: 'user_id',
+});
 
 // Product and DetailProduct (One-to-One)
-Product.hasOne(DetailProduct, { foreignKey: 'product_id' });
-DetailProduct.belongsTo(Product, { foreignKey: 'product_id' });
+product.hasOne(detail_product, { foreignKey: 'product_id' });
+detail_product.belongsTo(product, { foreignKey: 'product_id' });
 
 // Product and Media (One-to-Many)
-Product.hasMany(Media, { foreignKey: 'product_id' });
-Media.belongsTo(Product, { foreignKey: 'product_id' });
+product.hasMany(media, { foreignKey: 'product_id' });
+media.belongsTo(product, { foreignKey: 'product_id' });
