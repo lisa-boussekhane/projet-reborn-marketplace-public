0c4087461af78dae242a991d7c38d2447094e392
lisa-boussekhane <lisa.boussekhane@gmail.com>
2024-02-14 15:22:11 +0100

    update

--- BACK/App/Controllers/searchController.js
+++ BACK/App/Controllers/searchController.js
@@ -1,14 +1,15 @@
 const { Op } = require('sequelize');
-const { product } = require('../Models/product');
+const product = require('../Models/product');
 
 const searchController = {
   searchReborns: async (req, res) => {
     try {
-      const searchTerm = req.query.recherche;
+      const searchTerm = req.query.search;
+      console.log(searchTerm);
       const results = await product.findAll({
         where: {
           title: { [Op.iLike]: `%${searchTerm}%` },
-          kit_name: { [Op.iLike]: `%${searchTerm}%` },
+          // kit_name: { [Op.iLike]: `%${searchTerm}%` },
         },
       });
 
@@ -21,3 +22,30 @@ const searchController = {
 };
 
 module.exports = searchController;
+
+// const { Op, QueryTypes, sequelize } = require('sequelize');
+// const product = require('../Models/product');
+
+// const searchController = {
+//   searchReborns: async (req, res) => {
+//     try {
+//       const searchTerm = await sequelize.query('SELECT * FROM `product`', {
+//         type: QueryTypes.SELECT,
+//       });
+//       console.log(searchTerm);
+//       const results = await product.findAll({
+//         where: {
+//           title: { [Op.iLike]: `%${searchTerm}%` },
+//           // kit_name: { [Op.iLike]: `%${searchTerm}%` },
+//         },
+//       });
+
+//       res.status(200).json({ results });
+//     } catch (error) {
+//       console.log(error);
+//       res.status(500).json({ message: 'an unexpected error occured...' });
+//     }
+//   },
+// };
+
+// module.exports = searchController;
