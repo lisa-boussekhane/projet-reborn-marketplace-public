32886482faf813dfd203a65c47816b87ee143567
Sarah-Iritie <jarjenillustrations@gmail.com>
2024-02-15 13:49:01 +0100

    modif multer

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const jwt = require('jsonwebtoken');
 const passport = require('passport');
 const bcrypt = require('bcrypt');
 const router = require('./BACK/App/Router/router');
+const path = require('path');
+const multer = require('multer');
 
 const app = express();
 const port = process.env.PORT;
@@ -15,6 +17,24 @@ const port = process.env.PORT;
 // app.use('/router', authRoutes);
 // app.use('/protected', protectedRoute);
 
+app.set('view engine', 'ejs');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'Images');
+  },
+  filename: (req, file, cb) => {
+    console.log(file);
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/sellmyreborn', upload.single('image'), (req, res) => {
+  res.send('Image uploaded');
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
@@ -23,7 +43,6 @@ app.use(
     saveUninitialized: false,
   })
 );
-
 app.use(express.json());
 app.use(router);
 app.use(passport.initialize());
