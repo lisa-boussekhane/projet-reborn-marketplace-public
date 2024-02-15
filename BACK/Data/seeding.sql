81515f19fe1d304ef2b9407f1125788d1669f315
ritaverissimo86 <rita.verissimo.h@gmail.com>
2024-02-15 16:04:32 +0100

    guillemets simple sur short unique id

--- BACK/Data/seeding.sql
+++ BACK/Data/seeding.sql
@@ -45,10 +45,10 @@ INSERT INTO "shop" ("id", "name", "user_id") OVERRIDING SYSTEM VALUE VALUES
 ----------------------------------------------
 
 INSERT INTO "product" ("id", "security_code", "title", "kit_name", "sculptor", "size", "type", "weight", "age_range", "authenticity_card", "price", "shipping_fees", "user_id", "shop_id") OVERRIDING SYSTEM VALUE VALUES
-(1, 4f90d1, 'Super Realistic, Lifelike Alina Soft Weighted Body Reborn Baby Doll Girl', 'Alina', 'Linde Scherer', '20', 'Vinyl', '6', 'Baby', 'Yes', '500', '50', 3, 2),
-(2, a9d8f8, 'Sebastian Realistic Reborn Baby Boy', 'Sebastian', 'Olga Auer', '20', 'Vinyl', '7', 'Baby', 'Yes', '650', '50', 1, 1),
-(3, erlU3t, 'Baby Toddler Girl Lottie Has Realistic Skin Blue Eyes and Brown Hair', 'Lottie', 'Laura Lee Eagles', '24', 'Vinyl', '8', 'Toddler', 'Yes', '980', '60', 1, 1);
-(4, iM3i2Y, 'Baby Full Silicone Girl Johnnie Closed Eyes', 'Johnnie', 'Ina Volprich', '19.5', 'Silicone', '7', 'Baby', 'Yes', '1200', '60', 3, 1);
+(1, '4f90d1', 'Super Realistic, Lifelike Alina Soft Weighted Body Reborn Baby Doll Girl', 'Alina', 'Linde Scherer', '20', 'Vinyl', '6', 'Baby', 'Yes', '500', '50', 3, 2),
+(2, 'a9d8f8', 'Sebastian Realistic Reborn Baby Boy', 'Sebastian', 'Olga Auer', '20', 'Vinyl', '7', 'Baby', 'Yes', '650', '50', 1, 1),
+(3, 'erlU3t', 'Baby Toddler Girl Lottie Has Realistic Skin Blue Eyes and Brown Hair', 'Lottie', 'Laura Lee Eagles', '24', 'Vinyl', '8', 'Toddler', 'Yes', '980', '60', 1, 1);
+(4, 'iM3i2Y', 'Baby Full Silicone Girl Johnnie Closed Eyes', 'Johnnie', 'Ina Volprich', '19.5', 'Silicone', '7', 'Baby', 'Yes', '1200', '60', 3, 1);
 
 ON CONFLICT (unique_id) DO UPDATE;
 
