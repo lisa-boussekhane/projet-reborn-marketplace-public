4ac2a0dc49b91059ea04bbe48a9c5e69a7ce7a45
ritaverissimo86 <rita.verissimo.h@gmail.com>
2024-02-15 09:21:58 +0100

    update shop model

--- BACK/App/Models/shop.js
+++ BACK/App/Models/shop.js
@@ -1,25 +1,19 @@
 const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require('./sequelize');
+const sequelize = require ('./sequelize');
 
 class shop extends Model {}
 
-shop.init(
-  {
+shop.init({  
     name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false
     },
-    link_seller_profile: {
-      type: DataTypes.PATH,
-      allowNull: false,
-    },
-  },
-  {
+  
+  }, {  
     sequelize,
     modelName: 'shop',
     tableName: 'shop',
-  }
-);
-
-module.exports = shop;
+  });
+  
+  module.exports = shop;
