4e693b56d9b62cdc042a9fcdb9517e9a0add0f77
ritaverissimo86 <rita.verissimo.h@gmail.com>
2024-02-19 16:52:17 +0100

    middleware multer

--- BACK/App/Router/router.js
+++ BACK/App/Router/router.js
@@ -28,8 +28,8 @@ router.get('/result', searchController.searchReborns);
 router.post('/process-payment', verifyToken, paymentController.addStripePayment);
 
 router.get('/product/:id', productController.getProductPage);
-router.post('/product/create', verifyToken, productController.createProduct);
-router.patch('/product/:id', verifyToken, productController.updateProduct);
+router.post('/product/create', verifyToken, multerMiddleware, productController.createProduct);
+router.patch('/product/:id', verifyToken, multerMiddleware, productController.updateProduct);
 router.delete('/product/:id', verifyToken, productController.deleteProduct);
 router.get('/products', productController.getProductsPage);
 
