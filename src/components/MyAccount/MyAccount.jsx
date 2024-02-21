5b605484449810c5ba15957b7502ad7840d1d5c0
Sarah-Iritie <jarjenillustrations@gmail.com>
2024-02-21 09:26:55 +0100

    doublon myaccount

--- src/components/MyAccount/MyAccount.jsx
+++ src/components/MyAccount/MyAccount.jsx
@@ -85,26 +85,6 @@ export default function MyAccount() {
               />
             </label>
           </form>
-          <form className="profile__elem" method="get">
-            <label htmlFor="firstname">
-              First name <input type="text" name="firstname" id="firstname" />
-            </label>
-            <label htmlFor="last name">
-              Last name <input type="text" name="lastname" id="lastname" />
-            </label>
-            <label htmlFor="phone">
-              Phone number
-              <input
-                type="tel"
-                name="phone"
-                id="phone"
-                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                required
-              />
-            </label>
-
-            <input type="submit" value="Save" className="save__btn" />
-          </form>
         </div>
 
         <div className="login__information">
