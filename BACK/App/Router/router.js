5815552dcee9dc592c632a6992b717ffdbdac098
ritaverissimo86 <rita.verissimo.h@gmail.com>
2024-02-26 11:03:51 +0100

    function getAverageRating dans ratingController

--- BACK/App/Router/router.js
+++ BACK/App/Router/router.js
@@ -30,26 +30,12 @@ router.get('/results', searchController.searchReborns);
 
 router.post('/createorder', verifyToken, shopController.createOrder);
 
-router.post(
-  '/process-payment',
-  verifyToken,
-  paymentController.addStripePayment
-);
+router.post('/process-payment', verifyToken, paymentController.addStripePayment);
 
 router.get('/product/:id', productController.getOneProduct);
 router.get('/products', productController.getAllProducts);
-router.post(
-  '/product/:id',
-  verifyToken,
-  upload.array('photo', 12),
-  productController.createProduct
-);
-router.patch(
-  '/product/:id',
-  verifyToken,
-  upload.array('photo', 12),
-  productController.updateProduct
-);
+router.post('/product/:id', verifyToken, upload.array('photo', 12), productController.createProduct);
+router.patch('/product/:id', verifyToken, upload.array('photo', 12), productController.updateProduct);
 router.delete('/product/:id', verifyToken, productController.deleteProduct);
 
 // router.get('/products', productController.getProductsPage);
@@ -57,18 +43,8 @@ router.delete('/product/:id', verifyToken, productController.deleteProduct);
 router.get('/shop/:id', verifyToken, shopController.showShop);
 router.post('/createshop/:id', verifyToken, shopController.createShop);
 router.delete('/shop/:id', verifyToken, shopController.deleteShop);
-router.get(
-  '/user/orders/:id',
-  verifyToken,
-  shopController.getAllUserOrdersWithDetails
-);
-
-router.patch(
-  '/orders',
-  verifyToken,
-  uploadInvoice.single('invoice'),
-  shopController.uploadInvoiceInOrder
-);
+router.get('/user/orders/:id', verifyToken, shopController.getAllUserOrdersWithDetails);
+router.patch('/orders', verifyToken, uploadInvoice.single('invoice'), shopController.uploadInvoiceInOrder);
 
 router.get('/chat/:id', verifyToken, chatController.getMessage);
 router.get('/chat/:id', verifyToken, chatController.getAllMessages);
@@ -76,5 +52,6 @@ router.post('/chat/message/room/:id', verifyToken, chatController.sendMessage);
 
 router.get('shop/:id/ratings', ratingController.getShopRating);
 router.post('/shop/:id/rate', ratingController.postShopRating);
+router.get('shop/:id/average-rating', ratingController.getAverageRating);
 
 module.exports = router;
