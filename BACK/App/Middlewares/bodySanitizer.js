b88de5215ac7e31895fe078f7c23766dc679cf54
lisa-boussekhane <lisa.boussekhane@gmail.com>
2024-03-22 14:46:49 +0100

    adieu mon coucou2

--- BACK/App/Middlewares/bodySanitizer.js
+++ BACK/App/Middlewares/bodySanitizer.js
@@ -3,9 +3,10 @@ const sanitizer = require('sanitizer');
 const bodySanitizer = (req, res, next) => {
   if (req.body) {
     for (let property in req.body) {
-      console.log(property + ' = ' + req.body[property]);
+      // console.log('coucou je sanitize les données');
+      // console.log(property + ' = ' + req.body[property]);
       req.body[property] = sanitizer.escape(req.body[property]);
-      console.log(`After sanitization: ${property} = ${req.body[property]}`);
+      // console.log(`After sanitization: ${property} = ${req.body[property]}`);
     }
   }
   next();
